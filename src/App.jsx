9192d7c301d51b732a0874f4d250803eac7e4287
duong19122002 <duong19122002@gmail.com>
2022-03-11 10:25:05 +0700

    save: app

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,26 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+
+const ShowInfo = (props) => {
+  return <div>Show {props.name}</div>
 }
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useSate("green");
+  const [myStatus, setMySate] = useSate(false);
+  const [products, setProducts] = useSate([{ id: 1, name: "A" }, { id: 2, name: "B" }]);
+  return <div>
+    Number: {count}<br />
+    String <div> style={}</div>
+  </div>
+}
+
+
 
 export default App
