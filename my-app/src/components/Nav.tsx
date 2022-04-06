97582266168b7cfe0f6114f87781b4901a33504d
duong19122002 <duong19122002@gmail.com>
2022-04-06 21:33:52 +0700

    save

--- my-app/src/components/Nav.tsx
+++ my-app/src/components/Nav.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { list } from '../api/category'
 import { Category } from '../types/category'
+import '../css/nav.css'
 
 type Props = {}
 
@@ -18,9 +19,9 @@ const Nav = (props: Props) => {
       <div>
           <ul>
                 <li className="inline"><NavLink className="px-3 text-lg font-semibold" to="/">Trang chủ</NavLink></li>
-                <div className='relative inline'>
-                <li id="categories" className="inline"><a href="/" className="px-3 text-lg font-semibold py-16">Danh mục</a></li>
-                <ul className="sub_menu absolute left-0 z-50 bg-white">
+                <div className='dropdown relative inline'>
+                <button id="categories" className="dropbtn px-3 text-lg font-semibold py-16 inline">Danh mục <img className='w-3 inline' src="https://cdn-icons-png.flaticon.com/512/32/32195.png" alt="" /> </button>
+                <ul className="dropdown-content absolute left-0 z-50 bg-white">
                     {cate?.map((e,index) => {
                         return(
                             <li key={index} className="rounded-md"><a href={`/category/${e._id}`} className="pr-24 py-3 hover:bg-red-500 px-10 py-1 block text-lg font-semibold">{e.name}</a></li>
--- my-app/src/page/category.tsx
+++ my-app/src/page/category.tsx
@@ -7,25 +7,41 @@ import { ProductType } from '../types/product'
 type Props = {}
 
 const Categories = (props: Props) => {
-  const {id} = useParams();
+  const { id } = useParams();
   console.log(id)
-  const [cate,getcate] = useState<ProductType[]>([])
-   useEffect(()=>{
-    const category=async()=>{
-      const {data} = await getone(id)
+  const [cate, getcate] = useState<ProductType[]>([])
+  useEffect(() => {
+    const category = async () => {
+      const { data } = await getone(id)
       getcate(data.products)
-       }
-       category()
-   },[])
+    }
+    category()
+  }, [])
   return (
-    <div>
-      {cate?.map((e,index)=>{
-      return(
-      <h1 key={index}>{e.name}</h1>
-      )
-      }
-     )}
+    <section className="text-white-600 body-font">
+    <div className="container  mx-auto max-w-7x1 ">
+      <div className="flex flex-wrap w-full mb-4 p-4">
+        <div className="w-full mb-6 lg:mb-0">
+          <h1 className="sm:text-4xl text-5xl font-medium font-bold title-font mb-2 text-gray-900">Sản Phẩm Mới</h1>
+          <div className="h-1 w-20 bg-indigo-500 rounded" />
+        </div>
+      </div>
+      <div className="grid grid-cols-4 gap-4" >
+        {cate?.map((e, index) => {
+          return (
+
+            <div className="bg-white p-6 rounded-lg0 drop-shadow-xl" key={index}>
+              <img className="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src={`${e.img}`} alt="Image Size 720x400" />
+              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{e.name}</h3>
+              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">{e.price}</h2>
+              <p className="leading-relaxed text-base">{e.desc}</p>
+            </div>
+
+          )
+        })}
+      </div>
     </div>
+  </section>
   )
 }
 
